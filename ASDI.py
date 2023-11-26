252fb366f349024d8d6e0d802a1c5ffd50706d45
AlanESV <alaneduardo.santosvazquez@gmail.com>
2023-11-26 14:46:13 -0600

    Funcion Parse

--- ASDI.py
+++ ASDI.py
@@ -38,3 +38,65 @@ class ASDI:
     def buscar(self, aux):
         diccionario_indices = {"Q": 0, "D": 1, "P": 2, "A": 3, "A1": 4, "A2": 5, "A3": 6, "T": 7, "T1": 8, "T2": 9, "T3": 10}
         return diccionario_indices.get(aux, -1)
+
+def parse(self):
+        # Inicialización de la pila
+        pila = ["$", "Q"]
+
+        #se ejecuta hasta que el tope de la pila es el símbolo de fin de entrada ($).
+        while pila[-1] != "$":
+        
+            if pila[-1] == self.preanalisis.tipo:
+                # Si el tope de la pila coincide con el tipo del token actual
+                #se elimina de la pila y se avanza al siguiente token.
+                pila.pop()
+                self.i += 1
+                self.preanalisis = self.tokens[self.i]
+                #print("entre1", pila)
+
+            elif (pila[-1] in ["SELECT", "FROM", "DISTINCT", "ASTERISCO", "PUNTO", "COMA", "IDENTIFICADOR", "PUNTO", "$"]):
+                #verifica si el tope de la pila es un símbolo terminal 
+                self.hay_errores = True
+                #print("entre2", pila)
+
+            elif self.tabla[self.buscar(pila[-1])][self.preanalisis.buscar()] is None:
+                #verifica si la celda correspondiente en la tabla está vacía
+                self.hay_errores = True
+                #print("entre 3",pila)
+
+            elif self.tabla[self.buscar(pila[-1])][self.preanalisis.buscar()] is not None:
+                # significa que hay una regla gramatical definida en esa celda.
+                """print(
+                    f"{self.tabla[self.buscar(pila[-1])][self.preanalisis.buscar()].celda[0]}->",
+                    " ".join(self.tabla[self.buscar(pila[-1])][self.preanalisis.buscar()].celda[1:]),
+                )"""
+
+                celda_actual = self.tabla[self.buscar(pila[-1])][self.preanalisis.buscar()]
+
+                if celda_actual is not None:
+                    #Se extrae la producción de la regla gramatical, 
+                    # omitiendo el primer elemento que es el símbolo no terminal actual.
+                    produccion = celda_actual.celda[1:]
+
+                    #Se retira el símbolo no terminal actual de la pila.
+                    #Se agrega a la pila la producción de la regla gramatical, en orden inverso. 
+                    # excluyendo los epsilon.
+                    pila.pop()
+                    pila.extend(reversed([elem for elem in produccion if elem != "e"]))
+                    #print("entre 4.1",pila)
+
+                else:
+                    self.hay_errores = True
+
+            #print("al final",pila)
+
+            if self.hay_errores:
+                break
+        
+
+        if self.preanalisis.tipo == TipoToken.EOF and not self.hay_errores:
+            print("Consulta correcta")
+            return True
+        else:
+            print("Se encontraron errores")
+        return False
